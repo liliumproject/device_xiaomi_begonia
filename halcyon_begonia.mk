f83aa0fa6f7989734ce1c32ce840fea60d216b50
HeroBuxx <me@herobuxx.me>
2024-01-01 12:53:08 +0000

    begonia: Drop Viper Audio Support
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- halcyon_begonia.mk
+++ halcyon_begonia.mk
@@ -21,7 +21,6 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
