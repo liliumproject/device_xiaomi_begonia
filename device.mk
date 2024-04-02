e7aac3943906feade520e254a6fc797016dfc6da
bengris32 <bengris32@protonmail.ch>
2024-04-02 10:33:48 +0200

    begonia: Build `libwifi-hal-wrapper`
    Change-Id: I86e31ef7fe472941ba41181a66e6ee6e54d24570
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -441,6 +441,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libnl \
     hostapd \
+    libwifi-hal-wrapper \
     android.hardware.wifi@1.0.vendor \
     android.hardware.wifi@1.1.vendor \
     android.hardware.wifi@1.2.vendor \
